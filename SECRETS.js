a2cab214f9daa5e640c52a08ce71af72fa1abd41
BadTradersClub-Dev <badtradersclub@gmail.com>
2025-01-29 22:47:09 -0500

    Update SECRETS.js
    btc ai keys added

--- SECRETS.js
+++ SECRETS.js
@@ -1,10 +1,10 @@
 //? It is advisable to use environment variables instead of directly putting secrets in repository file but I have skipped this part as it would become complicated for many.
 //? Alternatively you can download the repository instead or forking and upload it from your account and keep it private, in that way, your secrets will not be exposed to the public.
 
-const APP_KEY = "mRUY3AgX7TOyI1RExZRQGsNAf";
-const APP_SECRET = "6FKqDpTUFNbZ5Kqt0Fyf4VdjAFsRESRCPUcRkGmIHOnOgXoLaq";
-const ACCESS_TOKEN = "1862313075530956800-K7jdpqjAvo3Ic6PUJccia3BwOBQwZK";
-const ACCESS_SECRET = "jT8SlEKl4S6w51HpyrARcQd2LaHWZGWxIV3gEn8IrVoy1";
+const APP_KEY = "iHsLFQBAuozKtgT9W1vTvxVD6";
+const APP_SECRET = "Kgob4k8OU86l4qVnlaHW9ZSvYLPEieVIDvcb2eqqgZ5bNwPlS6";
+const ACCESS_TOKEN = "1884802771926364160-HDptx5gyiqPxdJCgjYeAspCadQTlLD";
+const ACCESS_SECRET = "VLxEj98fVkFuQf0kLVkM0PLtSa25NkXzSwoT0x0qVYCWj";
 const GEMINI_API_KEY = "AIzaSyDVd9ajN78vy7e9HT5ObTl3m7hq4KIrols";
 
 const SECRETS = {
